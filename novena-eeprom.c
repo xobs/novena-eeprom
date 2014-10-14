69816c2c6807bb0ed100ccdbbe3918688ab5ca51
Sean Cross <xobs@kosagi.com>
2014-10-14 12:17:02 +0800

    eeprom: Update to v2
    novena-eeprom now reads and writes version 2 of the EEPROM mapping.  This
    allows for more expressive definition of modelines, and adds more features.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- novena-eeprom.c
+++ novena-eeprom.c
@@ -5,7 +5,6 @@
 #include <string.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
-#include <linux/i2c.h>
 #include <linux/i2c-dev.h>
 #include <ctype.h>
 
@@ -13,6 +12,11 @@
 
 #define EEPROM_ADDRESS (0xac>>1)
 #define I2C_BUS "/dev/i2c-2"
+
+union novena_eeprom_data {
+	struct novena_eeprom_data_v1	v1;
+	struct novena_eeprom_data_v2	v2;
+};
 
 struct eeprom_dev {
 	/* File handle to I2C bus */
@@ -25,7 +29,7 @@ struct eeprom_dev {
 	int				cached;
 
 	/* Contents of the EEPROM */
-	struct novena_eeprom_data 	data;
+	union novena_eeprom_data 	data;
 };
 
 int parse_features(char *str) {
@@ -85,7 +89,7 @@ int parse_mac(char *str, void *out) {
 int eeprom_read_i2c(struct eeprom_dev *dev, int addr, void *data, int count) {
 	struct i2c_rdwr_ioctl_data session;
 	struct i2c_msg messages[2];
-	uint8_t set_addr_buf[2];
+	char set_addr_buf[2];
 
 	memset(set_addr_buf, 0, sizeof(set_addr_buf));
 	memset(data, 0, count);
@@ -114,10 +118,10 @@ int eeprom_read_i2c(struct eeprom_dev *dev, int addr, void *data, int count) {
 	return 0;
 }
 
-int eeprom_write_i2c(struct eeprom_dev *dev, int addr, void *data, int count) {
+int eeprom_write_i2c(struct eeprom_dev *dev, int addr, const void *data, int count) {
 	struct i2c_rdwr_ioctl_data session;
 	struct i2c_msg messages[1];
-	uint8_t data_buf[2+count];
+	char data_buf[2+count];
 
 	data_buf[0] = addr>>8;
 	data_buf[1] = addr;
@@ -154,8 +158,75 @@ int eeprom_read(struct eeprom_dev *dev) {
 }
 
 int eeprom_write(struct eeprom_dev *dev) {
+	const char *buffer = (const char *)&dev->data;
+	unsigned int buffer_offset = 0;
+	int page_size = dev->data.v2.page_size;
+	int ret;
+
 	dev->cached = 1;
-	return eeprom_write_i2c(dev, 0, &dev->data, sizeof(dev->data));
+	while (buffer_offset < sizeof(dev->data)) {
+		if ((buffer_offset + page_size) > sizeof(dev->data))
+			page_size = sizeof(dev->data) - buffer_offset;
+
+		ret = eeprom_write_i2c(dev, buffer_offset,
+				       buffer + buffer_offset, page_size);
+		if (ret)
+			break;
+
+		buffer_offset += page_size;
+		usleep(10000);
+	}
+
+	return ret;
+}
+
+static int eeprom_export(struct eeprom_dev *dev, const char *filename) {
+	int fd;
+	int ret;
+
+	/* Ensure we have a cached copy */
+	eeprom_read(dev);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+	if (-1 == fd) {
+		perror("Unable to open file for exporting");
+		return 1;
+	}
+
+	ret = write(fd, &dev->data, sizeof(dev->data));
+	if (ret != sizeof(dev->data)) {
+		perror("Unable to export");
+		close(fd);
+		return 1;
+	}
+
+	close(fd);
+	return 0;
+}
+
+static int eeprom_import(struct eeprom_dev *dev, const char *filename) {
+	int fd;
+	int ret;
+
+	fd = open(filename, O_RDONLY);
+	if (-1 == fd) {
+		perror("Unable to open file for importing");
+		return 1;
+	}
+
+	ret = read(fd, &dev->data, sizeof(dev->data));
+	if (ret != sizeof(dev->data)) {
+		perror("Unable to import");
+		close(fd);
+		return 1;
+	}
+
+	close(fd);
+
+	/* Mark the copy as cached, so it won't get re-read */
+	dev->cached = 1;
+
+	return 0;
 }
 
 struct eeprom_dev *eeprom_open(char *path, int addr) {
@@ -185,6 +256,80 @@ malloc_err:
 	return NULL;
 }
 
+static void eeprom_get_defaults(struct eeprom_dev *dev) {
+	memset(&dev->data.v2, 0, sizeof(dev->data.v2));
+
+	memcpy(dev->data.v2.signature, NOVENA_SIGNATURE, sizeof(dev->data.v2.signature));
+
+	memset(&dev->data.v2.mac, 0xff, sizeof(dev->data.v2.mac));
+
+	dev->data.v2.version = 2;
+
+	dev->data.v2.features = feature_es8328 | feature_pcie | feature_gbit |
+		     feature_hdmi | feature_retina | feature_eepromoops;
+
+	dev->data.v2.eepromoops_offset = 4096;
+	dev->data.v2.eepromoops_length = 61440;
+
+	dev->data.v2.eeprom_size = 65536;
+	dev->data.v2.page_size = 128;
+
+	dev->data.v2.lvds1.frequency = 148500000;
+	dev->data.v2.lvds1.hactive = 1920;
+	dev->data.v2.lvds1.vactive = 1080;
+	dev->data.v2.lvds1.hback_porch = 148;
+	dev->data.v2.lvds1.hfront_porch = 88;
+	dev->data.v2.lvds1.hsync_len = 44;
+	dev->data.v2.lvds1.vback_porch = 36;
+	dev->data.v2.lvds1.vfront_porch = 4;
+	dev->data.v2.lvds1.vsync_len = 5;
+	dev->data.v2.lvds1.flags = vsync_polarity | hsync_polarity | data_width_8bit
+		      | mapping_jeida | dual_channel | channel_present;
+
+	dev->data.v2.lvds2.flags = channel_present;
+
+	/* Pull HDMI settings from e.g. EDID */
+	dev->data.v2.hdmi.flags = channel_present | ignore_settings | data_width_8bit;
+}
+
+static void eeprom_upgrade_v1_to_v2(struct eeprom_dev *dev) {
+	if (dev->data.v2.version != 1)
+		return;
+
+        dev->data.v2.features |= feature_eepromoops;
+
+        dev->data.v2.eepromoops_offset = 4096;
+        dev->data.v2.eepromoops_length = 61440;
+
+        dev->data.v2.eeprom_size = 65536;
+        dev->data.v2.page_size = 128;
+
+        if (dev->data.v2.features & feature_retina) {
+            dev->data.v2.lvds1.frequency = 148500000;
+            dev->data.v2.lvds1.hactive = 1920;
+            dev->data.v2.lvds1.vactive = 1080;
+            dev->data.v2.lvds1.hback_porch = 148;
+            dev->data.v2.lvds1.hfront_porch = 88;
+            dev->data.v2.lvds1.hsync_len = 44;
+            dev->data.v2.lvds1.vback_porch = 36;
+            dev->data.v2.lvds1.vfront_porch = 4;
+            dev->data.v2.lvds1.vsync_len = 5;
+            dev->data.v2.lvds1.flags = vsync_polarity | hsync_polarity
+				     | data_width_8bit | mapping_jeida
+				     | dual_channel | channel_present;
+
+            dev->data.v2.lvds2.flags = channel_present;
+        }
+
+        if (dev->data.v2.features & feature_hdmi) {
+		/* Pull HDMI settings from e.g. EDID */
+		dev->data.v2.hdmi.flags = channel_present | ignore_settings
+					| data_width_8bit;
+        }
+
+        dev->data.v2.version = 2;
+}
+
 int eeprom_close(struct eeprom_dev **dev) {
 	if (!dev || !*dev)
 		return 0;
@@ -207,7 +352,16 @@ int print_usage(char *name) {
 	"    -m    Specify a MAC address for the Gigabit Ethernet\n"
 	"    -s    Specify the device's serial number\n"
 	"    -f    A comma-delimited list of features present\n"
+	"    -o    Specify the EEPROM Oops start and size (e.g. -o 1000,3000)\n"
+	"    -p    EEPROM page size\n"
+	"    -l    EEPROM total size (length)\n"
+	"    -1    LVDS channel 1 modeline\n"
+	"    -2    LVDS channel 2 modeline\n"
+	"    -d    HDMI modeline\n"
 	"    -w    Actually write the value to the EEPROM\n"
+	"    -e    Export EEPROM to file\n"
+	"    -i    Import EEPROM from file\n"
+	"    -h    Print this help message\n"
 	"\n", name);
 
 	printf("Valid features:\n");
@@ -218,10 +372,64 @@ int print_usage(char *name) {
 	}
 	printf("\n");
 
+	printf("Modelines should be specified entirely in quotes.  Flags come at the end. You\n");
+	printf("may specify positive polarity with either the modesetting convention,\n");
+	printf("or as a flag.  E.g. either +HSync or hsync_polarity.  For example:\n");
+	printf("\n");
+	printf("    -1 'Modeline \"lvds1\" 148.500  1920 2068 2156 2200   1080 1116 1120 1125 +HSync +VSync channel_present dual_channel mapping_jeida data_width_8bit'\n");
+	printf("\n");
+
+	printf("Valid modeline flags:\n");
+	struct available_modesetting_flags *ms = available_modesetting_flags;
+	while (ms->name) {
+		printf("    %-25s%s\n", ms->name, ms->descr);
+		ms++;
+	}
+	printf("\n");
+
 	printf("Example:\n"
 		"  %s -f es8328,retina -s 12345 -w\n"
 		"", name);
 	return 0;
+}
+
+static void print_modesetting(struct modesetting *m, const char *name) {
+	printf("\t\tModeline \"%s\" %0.3f  %d %d %d %d   %d %d %d %d %cHSync %cVSync\n",
+		name,
+		m->frequency / 1000000.0,
+		m->hactive,
+		m->hactive + m->hback_porch,
+		m->hactive + m->hback_porch + m->hfront_porch,
+		m->hactive + m->hback_porch + m->hfront_porch + m->hsync_len,
+		m->vactive,
+		m->vactive + m->vback_porch,
+		m->vactive + m->vback_porch + m->vfront_porch,
+		m->vactive + m->vback_porch + m->vfront_porch + m->vsync_len,
+		(m->flags & hsync_polarity) ? '+' : '-',
+		(m->flags & vsync_polarity) ? '+' : '-');
+
+	printf("\t\tFlags: 0x%x", m->flags);
+	if (m->flags) {
+		int matched = 0;
+		int flags = m->flags;
+		struct available_modesetting_flags *feature = available_modesetting_flags;
+		while (feature->name) {
+			if (feature->flags & flags) {
+				if (!matched)
+					printf(" (%s", feature->name);
+				else
+					printf(",%s", feature->name);
+				matched++;
+				flags &= ~feature->flags;
+			}
+			feature++;
+		}
+		if (matched)
+			printf(")");
+		if (flags)
+			printf(" Unrecognized flags: 0x%02x", flags);
+	}
+	printf("\n");
 }
 
 int print_eeprom_data(struct eeprom_dev *dev) {
@@ -230,23 +438,23 @@ int print_eeprom_data(struct eeprom_dev *dev) {
 	if (ret)
 		return ret;
 
-	printf("\tSignature:   %c%c%c%c%c%c\n",
-			dev->data.signature[0],
-			dev->data.signature[1],
-			dev->data.signature[2],
-			dev->data.signature[3],
-			dev->data.signature[4],
-			dev->data.signature[5]);
-	printf("\tVersion:     %d\n", dev->data.version);
-	printf("\tSerial:      %d\n", dev->data.serial);
-	printf("\tMAC:         %02x:%02x:%02x:%02x:%02x:%02x\n",
-			dev->data.mac[0], dev->data.mac[1],
-			dev->data.mac[2], dev->data.mac[3],
-			dev->data.mac[4], dev->data.mac[5]);
-	printf("\tFeatures:    0x%x", dev->data.features);
-	if (dev->data.features) {
+	printf("\tSignature:        %c%c%c%c%c%c\n",
+			dev->data.v1.signature[0],
+			dev->data.v1.signature[1],
+			dev->data.v1.signature[2],
+			dev->data.v1.signature[3],
+			dev->data.v1.signature[4],
+			dev->data.v1.signature[5]);
+	printf("\tVersion:          %d\n", dev->data.v1.version);
+	printf("\tSerial:           %d\n", dev->data.v1.serial);
+	printf("\tMAC:              %02x:%02x:%02x:%02x:%02x:%02x\n",
+			dev->data.v1.mac[0], dev->data.v1.mac[1],
+			dev->data.v1.mac[2], dev->data.v1.mac[3],
+			dev->data.v1.mac[4], dev->data.v1.mac[5]);
+	printf("\tFeatures:         0x%x", dev->data.v1.features);
+	if (dev->data.v1.features) {
 		int matched = 0;
-		int flags = dev->data.features;
+		int flags = dev->data.v1.features;
 		struct feature *feature = features;
 		while (feature->name) {
 			if (feature->flags & flags) {
@@ -263,8 +471,82 @@ int print_eeprom_data(struct eeprom_dev *dev) {
 			printf(")");
 		if (flags)
 			printf(" Unrecognized flags: 0x%02x", flags);
-		printf("\n");
 	}
+	printf("\n");
+
+	if (dev->data.v2.version == 2) {
+		struct novena_eeprom_data_v2 *v2 = &dev->data.v2;
+
+		printf("\tEEPROM size:      %d\n", dev->data.v2.eeprom_size);
+		printf("\tEEPROM page size: %d\n", dev->data.v2.page_size);
+		printf("\tOops offset:      %d\n",
+					dev->data.v2.eepromoops_offset);
+		printf("\tOops length:      %d\n",
+					dev->data.v2.eepromoops_length);
+
+		printf("\tLVDS channel 1:\n");
+		print_modesetting(&v2->lvds1, "lvds1");
+		printf("\tLVDS channel 2:\n");
+		print_modesetting(&v2->lvds2, "lvds2");
+		printf("\tHDMI channel:\n");
+		print_modesetting(&v2->hdmi, "hdmi");
+	}
+	return 0;
+}
+
+static int parse_modesetting(struct modesetting *m, const char *arg) {
+	int len;
+	float mhz;
+	uint32_t h1, h2, h3, h4;
+	uint32_t v1, v2, v3, v4;
+	
+	sscanf(arg, "%*s %*s %f %u %u %u %u %u %u %u %d %n",
+			&mhz, &h1, &h2, &h3, &h4, &v1, &v2, &v3, &v4, &len);
+
+	m->frequency = mhz * 1000000;
+
+	m->hactive = h1;
+	m->hback_porch = h2 - m->hactive;
+	m->hfront_porch = h3 - m->hactive - m->hback_porch;
+	m->hsync_len = h4 - m->hactive - m->hback_porch - m->hfront_porch;
+
+	m->vactive = v1;
+	m->vback_porch = v2 - m->vactive;
+	m->vfront_porch = v3 - m->vactive - m->vback_porch;
+	m->vsync_len = v4 - m->vactive - m->vback_porch - m->vfront_porch;
+
+	m->flags = 0;
+
+	char flagstr[strlen(arg + len) + 1];
+	char *flag;
+	char *tmp = flagstr;
+
+	memcpy(flagstr, arg + len, sizeof(flagstr));
+
+	while ((flag = strtok(tmp, " ")) != NULL) {
+		tmp = NULL;
+
+		if (!strcasecmp(flag, "+hsync"))
+			m->flags |= hsync_polarity;
+		else if (!strcasecmp(flag, "-hsync"))
+			m->flags &= ~hsync_polarity;
+		if (!strcasecmp(flag, "+vsync"))
+			m->flags |= vsync_polarity;
+		else if (!strcasecmp(flag, "-vsync"))
+			m->flags &= ~vsync_polarity;
+		else {
+			struct available_modesetting_flags *feature = available_modesetting_flags;
+			while (feature->name) {
+				if (!strcasecmp(feature->name, flag)) {
+					m->flags |= feature->flags;
+					break;
+				}
+				feature++;
+			}
+		}
+
+	}
+
 	return 0;
 }
 
@@ -272,46 +554,105 @@ int main(int argc, char **argv) {
 	struct eeprom_dev *dev;
 	int ch;
 	int writing = 0;
-	int features;
-	int32_t serial;
-	uint8_t new_mac[6];
+	char *tmp;
 
+	struct novena_eeprom_data_v2 newrom;
+
+	int newdata = 0;
 	int update_mac = 0;
 	int update_features = 0;
 	int update_serial = 0;
+	int update_oops_start = 0;
+	int update_oops_length = 0;
+	int update_page_size = 0;
+	int update_total_size = 0;
+	int update_lvds1 = 0;
+	int update_lvds2 = 0;
+	int update_hdmi = 0;
 
 	dev = eeprom_open(I2C_BUS, EEPROM_ADDRESS);
 	if (!dev)
 		return 1;
 
-	while ((ch = getopt(argc, argv, "m:s:f:w")) != -1) {
+	while ((ch = getopt(argc, argv, "hm:s:f:wo:p:l:1:2:d:e:i:")) != -1) {
 		switch(ch) {
 
 		/* MAC address */
 		case 'm':
-			if (parse_mac(optarg, new_mac))
+			if (parse_mac(optarg, newrom.mac))
 				return 1;
 			update_mac = 1;
 			break;
 
 		/* Serial number */
 		case 's':
-			serial = strtoul(optarg, NULL, 0);
+			newrom.serial = strtoul(optarg, NULL, 0);
 			update_serial = 1;
 			break;
 
 		/* Featuresset */
 		case 'f':
-			features = parse_features(optarg);
-			if (features == -1)
+			newrom.features = parse_features(optarg);
+			if (newrom.features == -1)
 				return 1;
 			update_features = 1;
+			break;
+
+		case 'o':
+			newrom.eepromoops_offset = strtoul(optarg, &tmp, 0);
+			update_oops_start = 1;
+			if (tmp && *tmp) {
+				newrom.eepromoops_length = strtoul(tmp + 1,
+								    NULL, 0);
+				update_oops_length = 1;
+			}
+			break;
+
+		case 'p':
+			newrom.page_size = strtoul(optarg, NULL, 0);
+			update_page_size = 1;
+			break;
+
+		case 'l':
+			newrom.eeprom_size = strtoul(optarg, NULL, 0);
+			update_total_size = 1;
+			break;
+
+		case '1':
+			if (parse_modesetting(&newrom.lvds1, optarg))
+				return 1;
+			update_lvds1 = 1;
+			break;
+
+		case '2':
+			if (parse_modesetting(&newrom.lvds2, optarg))
+				return 1;
+			update_lvds2 = 1;
+			break;
+
+		case 'd':
+			if (parse_modesetting(&newrom.hdmi, optarg))
+				return 1;
+			update_hdmi = 1;
+			break;
+
+		case 'e':
+			return eeprom_export(dev, optarg);
+
+		case 'i':
+			if (eeprom_import(dev, optarg))
+				return 1;
+			newdata = 1;
 			break;
 
 		/* Write data */
 		case 'w':
 			writing = 1;
 			break;
+
+		case 'h':
+			print_usage(argv[0]);
+			return 1;
 
 		default:
 			printf("Unrecognized option: %c\n", ch);
@@ -323,10 +664,16 @@ int main(int argc, char **argv) {
 	argc -= optind;
 	argv += optind;
 
+	if (update_mac || update_serial || update_features ||
+		update_oops_start || update_oops_length ||
+		update_page_size || update_total_size ||
+		update_lvds1 || update_lvds2 || update_hdmi)
+		newdata = 1;
+
 	if (argc)
 		print_usage(argv[0]);
 	else if (!writing) {
-		if (update_mac || update_serial || update_features)
+		if (newdata)
 			printf("Not writing data, as -w was not specified\n");
 		printf("Current EEPROM settings:\n");
 		print_eeprom_data(dev);
@@ -336,14 +683,54 @@ int main(int argc, char **argv) {
 		ret = eeprom_read(dev);
 		if (ret)
 			return 1;
+		if (dev->data.v1.version == 1) {
+			printf("Updating v1 EEPROM to v2...\n");
+			eeprom_upgrade_v1_to_v2(dev);
+		}
+		else if (dev->data.v1.version == 2) {
+			/* Ignore v2 */;
+		}
+		else {
+			if (memcmp(dev->data.v2.signature, NOVENA_SIGNATURE,
+					sizeof(dev->data.v2.signature)))
+				printf("Blank EEPROM found, "
+					"setting defaults...\n");
+			else
+				fprintf(stderr,
+					"Unrecognized EEPROM version found "
+					"(v%d), overwriting with v2\n",
+					dev->data.v1.version);
+			eeprom_get_defaults(dev);
+		}
+
 		if (update_mac)
-			memcpy(&dev->data.mac, new_mac, sizeof(new_mac));
+			memcpy(&dev->data.v2.mac, newrom.mac, sizeof(newrom.mac));
 		if (update_serial)
-			dev->data.serial = serial;
+			dev->data.v2.serial = newrom.serial;
 		if (update_features)
-			dev->data.features = features;
-		memcpy(&dev->data.signature, NOVENA_SIGNATURE, sizeof(dev->data.signature));
-		dev->data.version = NOVENA_VERSION;
+			dev->data.v2.features = newrom.features;
+		if (update_oops_start)
+			dev->data.v2.eepromoops_offset = newrom.eepromoops_offset;
+		if (update_oops_length)
+			dev->data.v2.eepromoops_length = newrom.eepromoops_length;
+		if (update_page_size)
+			dev->data.v2.page_size = newrom.page_size;
+		if (update_total_size)
+			dev->data.v2.eeprom_size = newrom.eeprom_size;
+		if (update_lvds1)
+			memcpy(&dev->data.v2.lvds1, &newrom.lvds1,
+				sizeof(dev->data.v2.lvds1));
+		if (update_lvds2)
+			memcpy(&dev->data.v2.lvds2, &newrom.lvds2,
+				sizeof(dev->data.v2.lvds2));
+		if (update_hdmi)
+			memcpy(&dev->data.v2.hdmi, &newrom.hdmi,
+				sizeof(dev->data.v2.hdmi));
+		memcpy(&dev->data.v2.signature,
+				NOVENA_SIGNATURE,
+				sizeof(dev->data.v2.signature));
+
+		dev->data.v2.version = 2;
 
 		ret = eeprom_write(dev);
 		if (ret) {
--- novena-eeprom.h
+++ novena-eeprom.h
@@ -4,65 +4,191 @@
 #include <stdint.h>
 
 #define NOVENA_SIGNATURE "Novena"
-#define NOVENA_VERSION 1
+
+/* Bitmask polarities used as flag */
+enum modesetting_flags {
+
+	/* If 0, this display device is not present */
+	channel_present	= 0x01,
+
+	/* If this is lvds1 and this bit is set, use dual-channel LVDS */
+	dual_channel	= 0x02,
+
+	vsync_polarity	= 0x04,
+	hsync_polarity	= 0x08,
+
+	/* For LVDS, a value of 0 means PSWG, a value of 1 means JEIDA */
+	mapping_jeida	= 0x10,
+
+	/* If 0, channel is either 6-bit (LVDS) or 10-bit (HDMI) */
+	data_width_8bit	= 0x20,
+
+	/* Channel is present, but ignore EEPROM values and auto-detect them */
+	ignore_settings	= 0x40,
+};
+
+struct modesetting {
+	uint32_t	frequency;
+	uint16_t	hactive;
+	uint16_t	vactive;
+	uint16_t	hback_porch;
+	uint16_t	hfront_porch;
+	uint16_t	hsync_len;
+	uint16_t	vback_porch;
+	uint16_t	vfront_porch;
+	uint16_t	vsync_len;
+	uint32_t	flags;		/* enum modesetting_flags mask */
+} __attribute__((__packed__));
+
+#ifndef __cplusplus	/* C++ doesn't support named assignment */
+struct available_modesetting_flags {
+	uint32_t	flags;
+	char		*name;
+	char		*descr;
+} available_modesetting_flags[] = {
+	{
+		.name	= "channel_present",
+		.flags	= channel_present,
+		.descr	= "This channel is present",
+	},
+	{
+		.name	= "dual_channel",
+		.flags	= dual_channel,
+		.descr	= "Channel is dual-lane",
+	},
+	{
+		.name	= "vsync_polarity",
+		.flags	= vsync_polarity,
+		.descr	= "VSync polarity is positive",
+	},
+	{
+		.name	= "hsync_polarity",
+		.flags	= hsync_polarity,
+		.descr	= "HSync polarity is positive",
+	},
+	{
+		.name	= "mapping_jeida",
+		.flags	= mapping_jeida,
+		.descr	= "Use JEIDA (as opposed to PSWG) mapping",
+	},
+	{
+		.name	= "data_width_8bit",
+		.flags	= data_width_8bit,
+		.descr	= "Use 8-bit (as opposed to 6 [LVDS] or 10 [HDMI] bit)",
+	},
+	{
+		.name	= "ignore_settings",
+		.flags	= ignore_settings,
+		.descr	= "Ignore settings and attempt to auto-detect",
+	},
+	{} /* Sentinal */
+};
+#endif /* ! __cplusplus */
+
+enum feature_flags {
+	feature_es8328 		= 0x0001,
+	feature_senoko		= 0x0002,
+	feature_retina		= 0x0004,
+	feature_pixelqi		= 0x0008,
+	feature_pcie		= 0x0010,
+	feature_gbit		= 0x0020,
+	feature_hdmi		= 0x0040,
+	feature_eepromoops	= 0x0080,
+	feature_rootsrc_sata	= 0x0100,
+};
+
+#ifndef __cplusplus	/* C++ doesn't support named assignment */
+struct feature {
+	uint32_t	flags;
+	char		*name;
+	char		*descr;
+} features[] = {
+	{
+		.name	= "es8328",
+		.flags	= feature_es8328,
+		.descr	= "ES8328 audio codec",
+	},
+	{
+		.name	= "senoko",
+		.flags	= feature_senoko,
+		.descr	= "Senoko battery board",
+	},
+	{
+		.name	= "edp",
+		.flags	= feature_retina,
+		.descr	= "eDP bridge chip",
+	},
+	{
+		.name	= "pixelqi",
+		.flags	= feature_pixelqi,
+		.descr	= "PixelQi LVDS display (deprecated)",
+	},
+	{
+		.name	= "pcie",
+		.flags	= feature_pcie,
+		.descr	= "PCI Express support",
+	},
+	{
+		.name	= "gbit",
+		.flags	= feature_gbit,
+		.descr	= "Gigabit Ethernet",
+	},
+	{
+		.name	= "hdmi",
+		.flags	= feature_hdmi,
+		.descr	= "HDMI Output (deprecated)",
+	},
+	{
+		.name	= "eepromoops",
+		.flags	= feature_eepromoops,
+		.descr	= "EEPROM Oops storage",
+	},
+	{
+		.name	= "sataroot",
+		.flags	= feature_rootsrc_sata,
+		.descr	= "Root device is SATA",
+	},
+	{} /* Sentinal */
+};
+#endif /* ! __cplusplus */
 
 /*
  * For structure documentation, see:
  * http://www.kosagi.com/w/index.php?title=Novena/EEPROM
  */
-struct novena_eeprom_data {
+struct novena_eeprom_data_v1 {
 	uint8_t		signature[6];	/* 'Novena' */
-	uint8_t		version;	/* 1 */
-	uint8_t		reserved1;
-	uint32_t	serial;
-	uint8_t		mac[6];
-	uint16_t	features;
+	uint8_t		version;	/* always 1 */
+	uint8_t		reserved;
+	uint32_t	serial;		/* 32-bit serial number */
+	uint8_t		mac[6];		/* Gigabit MAC address */
+
+	/* Features present, from struct feature features[] below */
+	uint16_t	features;	/* Native byte order */
 } __attribute__((__packed__));
 
-struct feature {
-        uint32_t        flags;
-        char            *name;
-	char		*descr;
-};
+/* V1 is mostly a superset of v2, except the page size is a "reserved" field */
+struct novena_eeprom_data_v2 {
+	uint8_t		signature[6];	/* 'Novena' */
+	uint8_t		version;	/* always 2 */
+	uint8_t		page_size;	/* Size of EEPROM read/write page */
+	uint32_t	serial;		/* 32-bit serial number */
+	uint8_t		mac[6];		/* Gigabit MAC address */
 
-struct feature features[] = {
-	{
-		.name  = "es8328",
-		.flags = 0x01,
-		.descr = "ES8328 audio codec",
-	},
-	{
-		.name  = "senoko",
-		.flags = 0x02,
-		.descr = "Senoko battery board",
-	},
-	{
-		.name  = "retina",
-		.flags = 0x04,
-		.descr = "Retina-class dual-LVDS display",
-	},
-	{
-		.name  = "pixelqi",
-		.flags = 0x08,
-		.descr = "PixelQi LVDS display",
-	},
-	{
-		.name  = "pcie",
-		.flags = 0x10,
-		.descr = "PCI Express support",
-	},
-	{
-		.name  = "gbit",
-		.flags = 0x20,
-		.descr = "Gigabit Ethernet",
-	},
-	{
-		.name  = "hdmi",
-		.flags = 0x40,
-		.descr = "HDMI Output",
-	},
-	{} /* Sentinal */
-};
+	/* Features present, from struct feature features[] below */
+	uint16_t	features;	/* Native byte order */
 
+	/* Describes default resolutions of various output devices */
+	struct modesetting	lvds1;	/* LVDS channel 1 settings */
+	struct modesetting	lvds2;	/* LVDS channel 2 settings */
+	struct modesetting	hdmi;	/* HDMI settings */
+
+	/* An indicator of how large this particular EEPROM is */
+	uint32_t	eeprom_size;
+
+	/* If eepromoops is present, describes eepromoops storage */
+	uint32_t	eepromoops_offset;
+	uint32_t	eepromoops_length;
+} __attribute__((__packed__));
 
 #endif /* __NOVENA_EEPROM_H__ */
